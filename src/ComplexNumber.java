b0871346032a8801304e2c19afdc80e57166b13c
brian070069 <kkipngeno409@gmail.com>
2024-08-20 23:30:40 +0300

    initial commit

---
+++ src/ComplexNumber.java
@@ -0,0 +1,17 @@
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    private double getReal(){
+        return real;
+    }
+
+    private double getImaginary(){
+        return imaginary;
+    }
+}
